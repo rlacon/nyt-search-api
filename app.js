bec532a2b35d841667e8662be396bbddd16c2c4a
Ryan Lacon <ryan.lacon@gmail.com>
2019-07-09 13:30:42 -0400

    Typing a string in search box successfully searches for articles with that keyword

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
+
+
+
 $(".btn-search").on("click", function () {
-    var searchTerm = "election";
+    var searchTerm = $('#exampleFormControlInput1').val();
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=YTSDGMTlFa0R2Zy2g21ZJdYhicTreY7B";
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
+        console.log(response.response.docs);
     });
+
+    // $("#exampleFormControlInput1").val();
+
+
+
 });
