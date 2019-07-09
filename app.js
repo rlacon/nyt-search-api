8681a84cd4c5e629b55d1a2f1d76e073a86fd0b4
Ryan Lacon <ryan.lacon@gmail.com>
2019-07-09 13:11:15 -0400

    Setting up api call in JS file

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-var queryURL = ;
+$(".btn-search").on("click", function () {
+    var searchTerm = "election";
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=YTSDGMTlFa0R2Zy2g21ZJdYhicTreY7B";
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    });
 });
