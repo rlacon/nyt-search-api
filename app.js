0b5d2aecc8a04f794367162e11888f64bdb80b09
Ryan Lacon <ryan.lacon@gmail.com>
2019-07-09 13:59:21 -0400

    Clicking search now returns article headlines on the DOM

--- app.js
+++ app.js
@@ -9,11 +9,19 @@ $(".btn-search").on("click", function () {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response.response.docs);
+        // console.log(response.response.docs);
+        var results = response.response.docs;
+        // console.log(results);
+        for (var i = 0; i < results.length; i++) {
+            var articlesDiv = $("<div>");
+            // var p = $('<p>' + results[i].headline.main);
+            // articlesDiv.append(p);
+            articlesDiv.text(results[i].headline.main);
+            $(".articles").append(articlesDiv);
+            console.log(results[i].headline)
+        }
     });
-
-    // $("#exampleFormControlInput1").val();
-
-
-
 });
+
+
+// Display the articles (if the value is 5, return .this)
